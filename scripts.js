074b9ba23b57c1b2b1b4877677e1426fd58f4a90
Jephery Cyril <cyriljeph@gmail.com>
2023-09-09 09:13:24 +0100

    script_fixes_utct

--- scripts.js
+++ scripts.js
@@ -1,19 +1,15 @@
-// JavaScript to update the real-time timestamp, day of the week, and UTC time
 function updateData() {
     const dayOfWeekElement = document.getElementById('dayOfWeek');
     const utcTimeElement = document.getElementById('utcTime');
     const now = new Date();
     
-    // Update day of the week
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const dayOfWeek = daysOfWeek[now.getUTCDay()];
     dayOfWeekElement.textContent = dayOfWeek;
     
-    // Update UTC time in milliseconds
     const utcTime = now.getTime();
     utcTimeElement.textContent = utcTime;
 }
 
-// Update the data initially and then every second
 updateData();
 setInterval(updateData, 1000);
